fd5659a3ae15a719bc9a78651ed221e1330b6055
Kowshik Raj Durai Murugan <kowshik12raj@gmail.com>
2020-06-13 11:43:08 -0500

    Update BreastCancerClassification.py

--- BreastCancerClassification.py
+++ BreastCancerClassification.py
@@ -1,34 +1,8 @@
 ---------------------------------------------------------------------------------
 ### Project: BREAST CANCER CLASSIFICATION - SUPPORT VECTOR MACHINE CLASSIFIER ###
 ---------------------------------------------------------------------------------
----------------------------------
-### STEP 1: PROBLEM STATEMENT ###
----------------------------------
-'''
-- Predicting if the cancer diagnosis is benign or malignant based on several observations/features 
-          30 features are used, examples:
-        - radius (mean of distances from center to points on the perimeter)
-        - texture (standard deviation of gray-scale values)
-        - perimeter
-        - area
-        - smoothness (local variation in radius lengths)
-        - compactness (perimeter^2 / area - 1.0)
-        - concavity (severity of concave portions of the contour)
-        - concave points (number of concave portions of the contour)
-        - symmetry 
-        - fractal dimension ("coastline approximation" - 1)
-
-- Datasets are linearly separable using all 30 input features
-- Number of Instances: 569
-- Class Distribution: 212 Malignant, 357 Benign
-- Target class:
-         - Malignant
-         - Benign
-
-https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic)
-'''
 -----------------------------
-### STEP 2: IMPORTING DATA ###
+### STEP 1: IMPORTING DATA ###
 -----------------------------
 #import libraries 
 import pandas as pd             # Import Pandas for data manipulation using dataframes
@@ -55,7 +29,7 @@ df_cancer.head()
 df_cancer.tail()
 
 ------------------------------------
-### STEP 3: VISUALIZING THE DATA ###
+### STEP 2: VISUALIZING THE DATA ###
 ------------------------------------
 sns.pairplot(df_cancer, hue = 'target', vars = ['mean radius', 'mean texture', 'mean area', 'mean perimeter', 'mean smoothness'] )
 sns.countplot(df_cancer['target'], label = "Count")
@@ -68,7 +42,7 @@ plt.figure(figsize=(20,10))
 sns.heatmap(df_cancer.corr(), annot=True)
 
 -----------------------------------------------------------
-### STEP 4: MODEL TRAINING (FINDING A PROBLEM SOLUTION) ###
+### STEP 3: MODEL TRAINING (FINDING A PROBLEM SOLUTION) ###
 -----------------------------------------------------------
 #Let's drop the target label coloumns
 X = df_cancer.drop(['target'],axis=1)
@@ -90,7 +64,7 @@ svc_model = SVC()
 svc_model.fit(X_train, y_train)
 
 ------------------------------------
-### STEP 5: EVALUATING THE MODEL ###
+### STEP 4: EVALUATING THE MODEL ###
 ------------------------------------
 y_predict = svc_model.predict(X_test)
 cm = confusion_matrix(y_test, y_predict)
@@ -100,7 +74,7 @@ sns.heatmap(cm, annot=True)
 print(classification_report(y_test, y_predict))
 
 -----------------------------------
-### STEP 6: IMPROVING THE MODEL ###
+### STEP 5: IMPROVING THE MODEL ###
 -----------------------------------
 '''
 IMPROVING THE MODEL - PART 1
